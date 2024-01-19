ac0e0a85771eb716f2d269f64337032c062e3bfd
Joven Tan <jovenntann@gmail.com>
2024-01-19 10:41:48 +0800

    chore: Add gitignore, serverless config, and docs

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# google-trends
+## Installation and Deployment
+
+### Installing Serverless Plugins
+To install the necessary serverless plugins, run the following command:
+```bash
+serverless plugin install -n serverless-python-requirements
+serverless plugin install -n serverless-offline
+```
+
+### AWS Credentials Configuration
+Configure your AWS credentials by editing the `~/.aws/credentials` file. You can use `vi` or your preferred text editor:
+```bash
+vi ~/.aws/credentials
+```
+
+### Deploying to AWS
+To deploy the application to AWS with the `dev` stage, use the following command:
+```bash
+serverless deploy --stage dev
+```
+
+### Running Locally
+To run the application locally with the `dev` stage, use the following command:
+```bash
+serverless offline --stage dev
+```
---
+++ handler.py
@@ -0,0 +1,41 @@
+import os
+import json
+import requests
+import logging
+from urllib.parse import quote
+
+# Initialize logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    
+    serp_url = 'https://serpapi.com/search'
+    query = event.get('q', '')
+    date = event.get('date', 'today+5-y')
+    timezone = event.get('tz', '-540')
+    data_type = event.get('data_type', 'TIMESERIES')
+    api_key = os.environ.get('SERP_API_KEY')
+
+    logger.info(date)
+    
+    url = f"{serp_url}?engine=google_trends&q={quote(query)}&data_type={data_type}&date={date}&tz={timezone}&api_key={api_key}"
+    logger.info('Constructed URL: %s', url)
+
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        response_data = response.json()
+        logger.info('Request successful. Response data: %s', response_data)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response_data)
+        }
+    else:
+        logger.error('Request failed with status code: %s. Response text: %s', response.status_code, response.text)
+        return {
+            'statusCode': response.status_code,
+            'body': json.dumps(response.text)
+        }
