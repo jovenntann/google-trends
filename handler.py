5c84f7d823c06bbe3dc15fd75fe77fb7f587a7d7
Joven Tan <jovenntann@gmail.com>
2024-01-19 10:55:59 +0800

    refactor(handler): Update query param handling and logging

--- handler.py
+++ handler.py
@@ -11,16 +11,20 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     
+    logger.info('Received event: %s', event)
+
     serp_url = 'https://serpapi.com/search'
-    query = event.get('q', '')
-    date = event.get('date', 'today+5-y')
-    timezone = event.get('tz', '-540')
-    data_type = event.get('data_type', 'TIMESERIES')
+    query_parameters = event.get('queryStringParameters', {})
+    query = query_parameters.get('q', '')
+    date = query_parameters.get('date', 'today+5-y').replace(' ', '+')
+    timezone = query_parameters.get('tz', '-540')
+    data_type = query_parameters.get('data_type', 'TIMESERIES')
+    geo = query_parameters.get('geo', 'US')
     api_key = os.environ.get('SERP_API_KEY')
 
     logger.info(date)
     
-    url = f"{serp_url}?engine=google_trends&q={quote(query)}&data_type={data_type}&date={date}&tz={timezone}&api_key={api_key}"
+    url = f"{serp_url}?engine=google_trends&q={quote(query)}&data_type={data_type}&date={date}&tz={timezone}&geo={geo}&api_key={api_key}"
     logger.info('Constructed URL: %s', url)
 
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
