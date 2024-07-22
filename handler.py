c2cb97bd2c5f4923767588c2018450af37e62518
Joven Tan <jovenntann@gmail.com>
2024-07-22 16:33:52 +0800

    feat(yfinance): Add yfinance data retrieval endpoints

--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
 import os
 import json
-import requests
 import logging
-from urllib.parse import quote
+from datetime import datetime, timedelta
+import math
+import yfinance as yf
 
 # Initialize logger
 logger = logging.getLogger()
@@ -10,41 +11,119 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
-    
-    logger.info('Received event: %s', event)
+    logger.info(f"Event received: {event}")
+    logger.info(f"Context received: {context}")
 
-    serp_url = 'https://serpapi.com/search'
-    query_parameters = event.get('queryStringParameters', {})
-    query = query_parameters.get('q', '')
-    date = query_parameters.get('date', 'today+5-y').replace(' ', '+')
-    timezone = query_parameters.get('tz', '-540')
-    data_type = query_parameters.get('data_type', 'TIMESERIES')
-    geo = query_parameters.get('geo', 'US')
-    api_key = os.environ.get('SERP_API_KEY')
-
-    logger.info(date)
-    
-    url = f"{serp_url}?engine=google_trends&q={quote(query)}&data_type={data_type}&date={date}&tz={timezone}&geo={geo}&api_key={api_key}"
-    
-    logger.info('Constructed URL: %s', url)
-
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        response_data = response.json()
-        logger.info('Request successful. Response data: %s', response_data)
+    if event['httpMethod'] == 'GET' and event['path'] == '/status':
+        logger.info("Received GET request for /status")
+        content = {
+            "application": "Generate Report from yFinance Service",
+            "status": "OK",
+            "environment": os.environ.get('ENV_NAME', 'Unknown')
+        }
+        logger.info(f"Response content: {content}")
         return {
             'statusCode': 200,
-            'body': json.dumps(response_data),
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': True,
-            }
+            'body': json.dumps(content)
         }
-    else:
-        logger.error('Request failed with status code: %s. Response text: %s', response.status_code, response.text)
+
+    elif event['httpMethod'] == 'POST' and event['path'] == '/yfinance':
+        body = json.loads(event['body'])
+        isins = body['isins']
+        tickers = {}
+
+        for isin in isins:
+            data = yf.Ticker(isin)
+            try:
+                tickers[isin] = data.info['symbol']
+            except:
+                tickers[isin] = 'NONE'
+
+        date = body['date']
+        start_date = datetime.strptime(date, '%Y-%m-%d')
+        end_date = start_date + timedelta(days=1)
+        response = {}
+
+        for ticker in tickers:
+            try:
+                data = yf.download(tickers[ticker], start=str(start_date.date()), end=str(end_date.date()))
+                json_data = json.loads(json.dumps(list(data.T.to_dict().values())))
+                json_data = json_data[0]
+
+                if math.isnan(float(json_data['Open'])) or \
+                        math.isnan(float(json_data['High'])) or \
+                        math.isnan(float(json_data['Low'])) or \
+                        math.isnan(float(json_data['Close'])) or \
+                        math.isnan(float(json_data['Adj Close'])) or \
+                        math.isnan(float(json_data['Volume'])):
+
+                    formatted_data = {}
+
+                else:
+                    formatted_data = {
+                        'open': json_data['Open'],
+                        'high': json_data['High'],
+                        'low': json_data['Low'],
+                        'close': json_data['Close'],
+                        'adj_close': json_data['Adj Close'],
+                        'volume': json_data['Volume']
+                    }
+
+            except:
+                formatted_data = {}
+
+            response[ticker] = formatted_data
+
         return {
-            'statusCode': response.status_code,
-            'body': json.dumps(response.text)
+            'statusCode': 200,
+            'body': json.dumps(response)
+        }
+
+    elif event['httpMethod'] == 'POST' and event['path'] == '/yfinance/filter':
+        body = json.loads(event['body'])
+        tickers = body['tickers']
+        date = body['date']
+        start_date = datetime.strptime(date, '%Y-%m-%d')
+        end_date = start_date + timedelta(days=1)
+        response = {}
+
+        for ticker in tickers:
+            try:
+                data = yf.download(ticker, start=str(start_date.date()), end=str(end_date.date()))
+                json_data = json.loads(json.dumps(list(data.T.to_dict().values())))
+                json_data = json_data[0]
+
+                if math.isnan(float(json_data['Open'])) or \
+                        math.isnan(float(json_data['High'])) or \
+                        math.isnan(float(json_data['Low'])) or \
+                        math.isnan(float(json_data['Close'])) or \
+                        math.isnan(float(json_data['Adj Close'])) or \
+                        math.isnan(float(json_data['Volume'])):
+
+                    formatted_data = {}
+
+                else:
+                    formatted_data = {
+                        'open': json_data['Open'],
+                        'high': json_data['High'],
+                        'low': json_data['Low'],
+                        'close': json_data['Close'],
+                        'adj_close': json_data['Adj Close'],
+                        'volume': json_data['Volume']
+                    }
+            except Exception as e:
+                logger.error(e)
+                formatted_data = {}
+
+            response[ticker] = formatted_data
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response)
+        }
+
+    else:
+        return {
+            'statusCode': 404,
+            'body': json.dumps({'message': 'Not Found'})
         }
